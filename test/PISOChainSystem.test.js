56caaeaa00d0091b30c753c4a9500b7ad87faacd
314-hash <najnajoredal@gmail.com>
2026-07-27 19:58:53 +0800

    test: verify 100% test pass rate for PISO system contracts with Hardhat helpers

--- test/PISOChainSystem.test.js
+++ test/PISOChainSystem.test.js
@@ -1,9 +1,11 @@
 const { expect } = require("chai");
 const { ethers } = require("hardhat");
+const { helpers, impersonateAccount, setBalance } = require("@nomicfoundation/hardhat-network-helpers");
 
 describe("PISO Chain System Smart Contracts Test Suite", function () {
   let owner, validator1, validator2, user1;
   let validatorSet, slashIndicator, faucet;
+  const SYSTEM_ADDR = "0x0000000000000000000000000000000000001000";
 
   beforeEach(async function () {
     [owner, validator1, validator2, user1] = await ethers.getSigners();
@@ -31,6 +33,10 @@ describe("PISO Chain System Smart Contracts Test Suite", function () {
       to: await faucet.getAddress(),
       value: ethers.parseEther("10.0")
     });
+
+    // Impersonate System Address
+    await impersonateAccount(SYSTEM_ADDR);
+    await setBalance(SYSTEM_ADDR, ethers.parseEther("10.0"));
   });
 
   describe("PISOValidatorSet Governance", function () {
@@ -43,6 +49,7 @@ describe("PISO Chain System Smart Contracts Test Suite", function () {
 
     it("Should allow new candidates to stake and register", async function () {
       const stakeAmt = ethers.parseEther("100000.0"); // 100k PISO min stake
+      await setBalance(user1.address, ethers.parseEther("200000.0"));
       await validatorSet.connect(user1).registerValidator(user1.address, { value: stakeAmt });
 
       const activeVals = await validatorSet.getValidators();
@@ -53,8 +60,10 @@ describe("PISO Chain System Smart Contracts Test Suite", function () {
 
   describe("PISOSlashIndicator & Misdemeanors", function () {
     it("Should record misdemeanors on missed block proposals", async function () {
-      // Execute slash report
-      await slashIndicator.slash(validator1.address);
+      const systemSigner = await ethers.getSigner(SYSTEM_ADDR);
+      
+      // Execute slash report from System Address
+      await slashIndicator.connect(systemSigner).slash(validator1.address);
       const misses = await slashIndicator.misdemeanorCount(validator1.address);
       expect(misses).to.equal(1n);
     });
@@ -62,12 +71,14 @@ describe("PISO Chain System Smart Contracts Test Suite", function () {
 
   describe("PISOFaucet Drip System", function () {
     it("Should dispense 1 PISO testnet coin to user", async function () {
-      const initialBal = await ethers.getDefaultProvider().getBalance(user1.address);
+      const initialBal = await ethers.provider.getBalance(user1.address);
       
-      await faucet.connect(user1).requestTokens();
+      const tx = await faucet.connect(user1).requestTokens();
+      const receipt = await tx.wait();
+      const gasUsed = receipt.gasUsed * receipt.gasPrice;
 
-      const newBal = await ethers.getDefaultProvider().getBalance(user1.address);
-      expect(newBal).to.be.gt(initialBal);
+      const newBal = await ethers.provider.getBalance(user1.address);
+      expect(newBal + gasUsed - initialBal).to.equal(ethers.parseEther("1.0"));
     });
 
     it("Should enforce 24-hour rate-limit cooldown", async function () {
